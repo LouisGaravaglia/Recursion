64c4e08c1db5ee2189bb6eb5b0741b685a2c9cb2
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-10-31 16:20:31 -0700

    finished recursive implementation of binarySearch()

--- recursion.js
+++ recursion.js
@@ -59,14 +59,26 @@ function revString(str, i=str.length-1, newStr="") {
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {
-
+function gatherStrings(obj) {  
+  let stringArr = [];
+  for (let key of obj) {
+    if (typeof obj[key] === "string") stringArr.push(obj[key]);
+    if (typeof obj[key] === "object") stringArr.push(...gatherStrings(obj[key]));
+  }
+  return stringArr;
 }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {
+function binarySearch( arr, val, left = 0, right = arr.length ) {
+  //basecase
+  if ( left > right ) return -1;
+  //normalcase
+  let mid = Math.floor( ( right + left ) / 2 );
+  if (arr[mid] === val) return mid;
+  if (arr[mid] > val) return binarySearch( arr, val, left, right = mid-1 );
+  if (arr[mid] < val) return binarySearch( arr, val, left = mid + 1, right );
 
 }
 
